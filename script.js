c4b8631de280683fc3d19108d19077ddc112a0a7
Loner-dot <emmanuelitse7@gmail.com>
2026-03-29 16:36:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function typeText(element, text, speed = 40) {
+  let i = 0;
+  function typing() {
+    if (i < text.length) {
+      element.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typing, speed);
+    }
+  }
+  typing();
+}
